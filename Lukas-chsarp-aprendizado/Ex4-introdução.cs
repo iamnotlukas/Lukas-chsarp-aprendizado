0ccaeb9f84390d7f2b0cc5060059e42b779a8db2
Lucas Andrade <fulturosoldado@gmail.com>
2023-04-07 15:39:34 -0300

    realização ex4 - introdução

--- Lukas-chsarp-aprendizado/Ex4-introdução.cs
+++ Lukas-chsarp-aprendizado/Ex4-introdução.cs
@@ -14,13 +14,20 @@ namespace Lukas_chsarp_aprendizado
             inicio:
             Console.Clear();
             double n1, n2, n3, media;
+            Console.ForegroundColor = ConsoleColor.Green;   
             Console.WriteLine("Digite o numero 1: ");
+            Console.ForegroundColor= ConsoleColor.Blue;
             n1=Convert.ToDouble(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Digite o segudo número: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
             n2=Convert.ToDouble(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Digite o terceiro número: ");
-            n3=Convert.ToDouble(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Blue;
+            n3 =Convert.ToDouble(Console.ReadLine());
             media = (n1 + n2 + n3) / 3;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("A média dos seus número é: "+ media);
             Console.ReadKey();
             goto inicio;

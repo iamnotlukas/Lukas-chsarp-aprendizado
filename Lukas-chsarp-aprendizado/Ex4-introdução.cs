21881446a94c1a21eee38a648ec6e9ab2d83dee6
Lucas Andrade <fulturosoldado@gmail.com>
2023-04-07 15:59:27 -0300

    Realização exercício 5

--- Lukas-chsarp-aprendizado/Ex4-introdução.cs
+++ Lukas-chsarp-aprendizado/Ex4-introdução.cs
@@ -9,26 +9,18 @@ namespace Lukas_chsarp_aprendizado
     internal class Program
     {
         static void Main(string[] args)
-        //4.  Elaborar um programa e fluxograma para calcular a média aritmética de três  números digitados pelo usuário.
+        //5) Escreva um código para ler um valor (do teclado) e escrever (na tela) o seu antecessor.
         {//inicio
-            inicio:
+        inicio:
             Console.Clear();
-            double n1, n2, n3, media;
-            Console.ForegroundColor = ConsoleColor.Green;   
-            Console.WriteLine("Digite o numero 1: ");
-            Console.ForegroundColor= ConsoleColor.Blue;
-            n1=Convert.ToDouble(Console.ReadLine());
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Digite o segudo número: ");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            n2=Convert.ToDouble(Console.ReadLine());
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Digite o terceiro número: ");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            n3 =Convert.ToDouble(Console.ReadLine());
-            media = (n1 + n2 + n3) / 3;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("A média dos seus número é: "+ Math.Round(media,3));
+            double n1, n1_n;
+            Console.WriteLine("Digite seu número: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            n1_n = n1 - 1;
+            Console.WriteLine("O antecessor do seu número é: " + n1_n);
+
+
+
             Console.ReadKey();
             goto inicio;
         }//fim

2f6ae262b45c56b3c02c676b52f6f3c79d3830d9
Lucas Andrade <fulturosoldado@gmail.com>
2023-04-08 17:25:19 -0300

    Gerador de email completo

--- Lukas-csharp-aprendizado/Lukas-csharp-aprendizado/Program.cs
+++ Lukas-csharp-aprendizado/Lukas-csharp-aprendizado/Program.cs
@@ -13,21 +13,20 @@ namespace Lukas_csharp_aprendizado
         static void Main(string[] args)
         //Crie um gerador de e-mail que solicite o nome do usuário completo e mostre a o final o email:nome@fatec.sp.gov.br  
         {//inicio
-            string nome, nome_i;
-            int posicao;
-            inicio:
             Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Blue;
+            inicio:
+            string nome, nome_i, sobrenome_i;
+            int posicao, posicao2;
             Console.WriteLine("Digite seu nome completo: ");
             nome = Console.ReadLine();
             nome = nome.ToLower();
-            posicao = nome.LastIndexOf(" ");
-            nome_i = nome.Substring(posicao + 1);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("O Seu email institucional é: " + nome_i + "@fatec.sp.gov.br");
-
-            
-
+            posicao = nome.IndexOf(" ");
+            posicao2 = nome.LastIndexOf(" ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            nome_i = nome.Substring(0,posicao);
+            sobrenome_i = nome.Substring(posicao2 + 1);
+            Console.WriteLine("Seu email institucional é: " + nome_i + "." + sobrenome_i + "@fatec.sp.gov.br");
+           
             Console.ReadKey();
             goto inicio;
         }//fim

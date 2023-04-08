eaf902980a6163699472ef7125efb9784c6613b8
Lucas Andrade <fulturosoldado@gmail.com>
2023-04-08 20:59:56 -0300

    Leitor de produto de mercado

--- Lukas-csharp-aprendizado/Lukas-csharp-aprendizado/Program.cs
+++ Lukas-csharp-aprendizado/Lukas-csharp-aprendizado/Program.cs
@@ -11,22 +11,50 @@ namespace Lukas_csharp_aprendizado
     internal class Program
     {
         static void Main(string[] args)
-        //Crie um gerador de e-mail que solicite o nome do usuário completo e mostre a o final o email:nome@fatec.sp.gov.br  
+        //3 - Faça um programa para ler: a descrição do produto(nome), a quantidade adquirida e o preço unitário.Calcular e escrever o total(total = quantidade adquirida* preço unitário), o desconto e o total a pagar(total a pagar = total - desconto), sabendo-se que:
+        //- Se quantidade <= 5 o desconto será de 2%
+        //- Se quantidade > 5 e quantidade <=10 o desconto será de 3%
+        //- Se quantidade > 10 o desconto será de 5%
         {//inicio
-            Console.Clear();
-            inicio:
-            string nome, nome_i, sobrenome_i;
-            int posicao, posicao2;
-            Console.WriteLine("Digite seu nome completo: ");
+        inicio:
+            string nome;
+            double qnt, des, preço, total, total_d;
+            Console.Write("Digite o nome do produto: ");
             nome = Console.ReadLine();
-            nome = nome.ToLower();
-            posicao = nome.IndexOf(" ");
-            posicao2 = nome.LastIndexOf(" ");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            nome_i = nome.Substring(0,posicao);
-            sobrenome_i = nome.Substring(posicao2 + 1);
-            Console.WriteLine("Seu email institucional é: " + nome_i + "." + sobrenome_i + "@fatec.sp.gov.br");
-           
+            Console.Write("Digite o preço do produto: ");
+            preço = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a quantidade adquirida: ");
+            qnt = Convert.ToDouble(Console.ReadLine());
+
+            if (qnt <= 5)
+            {
+               
+                Console.WriteLine("Seu desconto é de 2%");
+                total = preço * qnt;
+                Console.WriteLine("Total a pagar sem desconto: " + total);
+                total_d = total * 0.02;
+
+                Console.WriteLine("total do valor do " + nome + " com desconto: " + total_d);
+            }
+
+            else if (qnt > 5 && qnt <=10)
+            {
+                Console.WriteLine("Seu desconto é de 3%");
+                total = preço * qnt;
+                Console.WriteLine("Total a pagar sem desconto: " + total);
+                total_d = total * 0.03;
+                Console.WriteLine("total do valor do "+ nome+" com desconto: " + total_d);
+
+            }
+            else if (qnt > 10)
+            {
+                Console.WriteLine("Seu desconto é de 5%");
+                total = preço * qnt;
+                Console.WriteLine("Total a pagar sem desconto: " + total);
+                total_d = total * 0.05;
+                Console.WriteLine("total do valor do " + nome + " com desconto: " + total_d);
+            }
+
             Console.ReadKey();
             goto inicio;
         }//fim
